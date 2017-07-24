83b40fbf6b777d885dc02a87191606a36f3e4efe
Lee Cheneler <leecheneler1@hotmail.co.uk>
2017-07-25 00:44:41 +0100

    sorted out variables and completed select styling

--- src/components/demos/FormDemo/index.jsx
+++ src/components/demos/FormDemo/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FormDemo = () =>
   <div>
     <div className="ea-u-soft-bottom">
-      <h3>Textboxes</h3>
+      <h3>Text</h3>
       <div className="ea-o-grid ea-o-grid--gutter ea-o-grid--gutter ea-o-grid--trench">
         <div className="ea-o-grid__item ea-u-1/2@xs">
           <label htmlFor="firstName">First name:</label>
@@ -22,8 +22,8 @@ const FormDemo = () =>
           <textarea id="bio" className="c-textbox" placeholder="Bio" />
         </div>
       </div>
-      <h3>Checkbox</h3>
-      <ul className="ea-o-list ea-o-list--bare ea-o-list--trench">
+      <h5>Checkbox</h5>
+      <ul className="ea-o-list ea-o-list--bare ea-o-list--trench ea-o-list--gutter ea-o-list--inline">
         <li className="ea-o-list__item">
           <div className="c-input">
             <input className="c-input__input" id="cb_enabled" type="checkbox" />
@@ -43,8 +43,8 @@ const FormDemo = () =>
           </div>
         </li>
       </ul>
-      <h3>Radio List</h3>
-      <ul className="ea-o-list ea-o-list--bare ea-o-list--trench">
+      <h5>Radio List</h5>
+      <ul className="ea-o-list ea-o-list--bare ea-o-list--trench ea-o-list--gutter ea-o-list--inline">
         <li className="ea-o-list__item">
           <div className="c-input">
             <input className="c-input__input" id="rb_enabled_1" type="radio" name="group" />
@@ -64,7 +64,37 @@ const FormDemo = () =>
           </div>
         </li>
       </ul>
+      <h5>Select</h5>
+      <div className="ea-o-grid ea-o-grid--gutter">
+        <div className="ea-o-grid__item ea-u-1/3@sm">
+          <label htmlFor="select_1">Select:</label>
+          <select id="select_1" className="c-select">
+            <option>Select an option</option>
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+          </select>
+        </div>
+        <div className="ea-o-grid__item ea-u-1/3@sm">
+          <label htmlFor="select_2">Small Select:</label>
+          <select id="select_2" className="c-select c-select--small">
+            <option>Select an option</option>
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+          </select>
+        </div>
+        <div className="ea-o-grid__item ea-u-1/3@sm">
+          <label htmlFor="select_3">Disabled Select:</label>
+          <select id="select_3" className="c-select" disabled>
+            <option>Select an option</option>
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+          </select>
+        </div>
+      </div>
     </div>
-  </div>
+  </div >
 
 export default FormDemo
